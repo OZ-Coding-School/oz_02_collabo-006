590624344136ce874b4de2c49ffdd9d6eb0d18d4
lsh7465 <lsh746527@gmail.com>
2024-04-30 03:57:51 +0900

    [Docs] 마이페이지, 내아카이브페이지 파일 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/index';
 import './App.css';
 import SignUpPage from 'pages/SignUpPage';
+import MyArchive from 'pages/MyArchive/indes';
+import MyPage from 'pages/MyPage';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/myarchive" element={<MyArchive />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </div>
   );
