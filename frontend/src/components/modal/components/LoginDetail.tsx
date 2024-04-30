1351f13a01b76ed490b920f641ea74fc0943b59c
lsh7465 <lsh746527@gmail.com>
2024-04-30 13:50:02 +0900

    [Docs] 약관동의폼 삭제

--- frontend/src/components/modal/components/LoginDetail.tsx
+++ frontend/src/components/modal/components/LoginDetail.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const ModalBody = styled.div`
   display: flex;
@@ -8,12 +9,15 @@ const ModalBody = styled.div`
   width: 25vw;
   margin: 0 auto;
 `;
+
 const InputWrap = styled.div`
   margin-top: 24px;
 `;
+
 const SbodyText = styled.span`
   font-size: 16px;
 `;
+
 const SInput = styled.input`
   width: 25vw;
   height: 5.5vh;
@@ -25,12 +29,15 @@ const SInput = styled.input`
   padding: 16px;
   font-size: 16px;
 `;
+
 const SforgotPW = styled.span`
   margin-top: 12px;
   font-size: 14px;
   color: #756982;
   white-space: nowrap;
+  align-self: flex-start;
 `;
+
 const SloginButton = styled.button`
   width: 25vw;
   height: 40px;
@@ -44,6 +51,7 @@ const SloginButton = styled.button`
   border-radius: 20px;
   border: none;
 `;
+
 const SignUpTitle = styled.button`
   text-align: center;
   margin-top: 12px;
@@ -53,7 +61,7 @@ const SignUpTitle = styled.button`
   border: 0;
   background-color: transparent;
   &:hover {
-    color: #b88cde;
+    color: #b98ce0;
     font-weight: bold;
   }
   &:active {
@@ -61,6 +69,7 @@ const SignUpTitle = styled.button`
     font-weight: bold;
   }
 `;
+
 const ModalTitle = styled.span`
   display: flex;
   justify-content: center;
@@ -68,24 +77,26 @@ const ModalTitle = styled.span`
   font-size: 22px;
   font-weight: bold;
 `;
-interface LoginModalProps {
-  onSignUpClick: () => void;
-}
-const LoginModal = ({ onSignUpClick }: LoginModalProps) => (
-  <ModalBody>
-    <ModalTitle>Login</ModalTitle>
-    <InputWrap>
-      <SbodyText>ID</SbodyText>
-      <SInput placeholder="아이디" />
-    </InputWrap>
-    <InputWrap>
-      <SbodyText>Password</SbodyText>
-      <SInput placeholder="비밀번호" />
-    </InputWrap>
-    <SforgotPW>비밀번호를 잊어버리셨나요?</SforgotPW>
-    <SloginButton>Login</SloginButton>
-    <SignUpTitle onClick={onSignUpClick}>회원 가입</SignUpTitle>
-  </ModalBody>
-);
+
+const LoginModal = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ModalBody>
+      <ModalTitle>Login</ModalTitle>
+      <InputWrap>
+        <SbodyText>ID</SbodyText>
+        <SInput placeholder="아이디" />
+      </InputWrap>
+      <InputWrap>
+        <SbodyText>Password</SbodyText>
+        <SInput placeholder="비밀번호" />
+      </InputWrap>
+      <SforgotPW>비밀번호를 잊어버리셨나요?</SforgotPW>
+      <SloginButton>Login</SloginButton>
+      <SignUpTitle onClick={() => navigate('/signup')}>회원 가입</SignUpTitle>
+    </ModalBody>
+  );
+};
 
 export default LoginModal;
