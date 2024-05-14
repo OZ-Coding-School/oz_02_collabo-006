6e9d1b70ba1a8d4ae287e6069bb9915883483fb8
lsh7465 <lsh746527@gmail.com>
2024-05-14 19:02:40 +0900

    Feat : 해쉬태그 UI기능 추가, Refactor : 코드 위치 정리, Add : delete 아이콘 추가

--- frontend/src/pages/CreatePostPage/index.tsx
+++ frontend/src/pages/CreatePostPage/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import {
   LIGHT_GRAY,
@@ -12,6 +12,7 @@ import { activeStyles, hoverStyles } from 'constant/buttonPseudoClass';
 import { CREATE_POSTS } from 'constant/endPoint';
 import HashTagButton, { buttonLists } from './hashTagButton';
 import { ReactComponent as ToggleArrowIcon } from '../../asset/toggle-arrow.svg';
+import { ReactComponent as DeleteIcon } from '../../asset/delete.svg';
 import axios from 'axios';
 
 const CreatePostHeader = styled.div`
@@ -46,11 +47,16 @@ const FormTitle = styled.h1`
   text-align: center;
   margin-top: 20px;
 `;
-
+const ImageCount = styled.span`
+  float: right;
+  margin: 0 5px 8px 0;
+  font-size: 14px;
+  font-weight: bold;
+  color: #756982;
+`;
 const UploadImageArea = styled.div`
   width: 100%;
-  height: auto;
-  min-height: 24vh;
+  min-height: 200px;
   border: 2px dashed ${INPUT_LIGHTGRAY};
   border-radius: 12px;
   margin: 16px 0 16px 0;
@@ -61,7 +67,8 @@ const UploadImageArea = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
 `;
-const HiddenI = styled.div`
+
+const HiddenImageContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   flex-wrap: wrap;
@@ -93,7 +100,23 @@ const UploadImageSmallText = styled.span`
 const HiddenImageInput = styled.input`
   display: none;
 `;
+const ImageDisplay = styled.div`
+  position: relative;
+  width: 90px;
+  height: 90px;
+  overflow: hidden;
+  margin: 3.5px;
+  transition: 0.2s ease-in-out;
+  border-radius: 5px;
 
+  &:hover {
+    transform: scale(1.02);
+  }
+`;
+const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
 const ContentsAndHashTagContainer = styled.div``;
 
 const UploadContents = styled.textarea`
@@ -141,26 +164,11 @@ const BaseLabel = styled.label`
 const ContentsTitle = styled(BaseLabel)``;
 const HashTagTitle = styled(BaseLabel)``;
 
-const ImageDisplay = styled.div`
-  position: relative;
-  width: 90px;
-  height: 90px;
-  overflow: hidden;
-  border: 1px solid #ccc;
-  margin: 3.5px;
-  transition: 0.2s ease-in-out;
-
-  &:hover {
-    transform: scale(1.02);
-  }
-`;
-
-const DeleteIcon = styled.span`
+const DeleteIconWrap = styled.i`
   position: absolute;
   top: 0;
   right: 0;
   cursor: pointer;
-  background: rgba(255, 0, 0, 0.6);
   width: 20px;
   height: 20px;
   display: flex;
@@ -168,7 +176,6 @@ const DeleteIcon = styled.span`
   justify-content: center;
   color: white;
   transition: 0.2s ease-in-out;
-  font-size: 16px;
   &:hover {
     transform: scale(1.15);
   }
@@ -180,7 +187,7 @@ const DeleteIcon = styled.span`
 const HashtagCreate = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-top: 6px;
+  margin: 6px 0;
 `;
 const TagButton = styled.button`
   background-color: ${LIGHT_PURPLE};
@@ -226,12 +233,12 @@ const HashTagToggleIconWrap = styled.button`
 `;
 
 interface toggleType {
-  isRotated: boolean;
+  $isRotated: boolean;
 }
 const HashTagToggleIcon = styled.i<toggleType>`
   height: 12px;
   display: inline-block;
-  transform: ${(props) => `rotate(${props.isRotated ? '180deg' : '0deg'})`};
+  transform: ${(props) => `rotate(${props.$isRotated ? '180deg' : '0deg'})`};
   transition: transform 0.3s ease;
   transform-origin: center center;
   vertical-align: middle;
@@ -258,14 +265,109 @@ const CreatePostPage = () => {
   const [activeTags, setActiveTags] = useState<string[]>([]); // 추가한 해시태그와 해시태그가 보관되는 곳
   const [showWarning, setShowWarning] = useState(false);
   const [warningMessage, setWarningMessage] = useState('');
-  const [isRotated, setIsRotated] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);  // 해시태그 버튼 컨테이너의 표시 상태
+  const [isRotated, setIsRotated] = useState(false); // 아이콘 회전
+  const [isOpen, setIsOpen] = useState(false); // 해시태그 보이기/숨기기
+  const [isFirstChange, setIsFirstChange] = useState(true); // 해시태그가 처음 반응할때만
 
-  const toggleHashTagVisibility = () => {
-    setIsOpen(!isOpen);  // 표시 상태 토글
+  useEffect(() => {
+    // 마지막에 추가된 태그를 확인
+    const lastAddedTag = activeTags[activeTags.length - 1];
+
+    // 마지막에 추가된 태그가 buttonLists에 속하지 않고, isFirstChange가 true일 경우에만 실행
+    if (
+      activeTags.length > 0 &&
+      isFirstChange &&
+      !buttonLists.includes(lastAddedTag)
+    ) {
+      setIsOpen(!isOpen); // isOpen 상태를 토글
+      setIsFirstChange(false); // 한 번 실행 후 더 이상 실행되지 않도록
+    }
+  }, [activeTags, isFirstChange, isOpen]);
+
+  // ## 이미지 업로드 관련 부분 ##
+  // 이미지 업로드 처리
+  const handleUploadClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (imageSrc.length >= 10) {
+      alert('최대 10장까지만 업로드 가능합니다.');
+      return;
+    }
+    fileInputRef.current?.click();
   };
-  const toggleRotation = () => {
-    setIsRotated(!isRotated);
+  // 이미지 리사이징
+  const resizeImage = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const maxDimension = 90; // 최대 너비와 높이
+        let newWidth, newHeight;
+
+        if (img.width > img.height) {
+          // 너비가 높이보다 클 경우, 너비를 기준으로 비율 조정
+          newWidth = maxDimension;
+          newHeight = (img.height / img.width) * maxDimension;
+        } else {
+          // 높이가 너비보다 크거나 같을 경우, 높이를 기준으로 비율 조정
+          newHeight = maxDimension;
+          newWidth = (img.width / img.height) * maxDimension;
+        }
+
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+        // 캔버스에 새로 계산된 크기로 이미지를 그립니다
+        ctx.drawImage(img, 0, 0, newWidth, newHeight);
+        resolve(canvas.toDataURL());
+      };
+      img.onerror = reject;
+      img.src = URL.createObjectURL(file);
+    });
+  };
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const fileArray = Array.from(files);
+      const newFileNames = fileArray.map((file) => file.name); // 파일 이름 추출
+
+      setFileNames(newFileNames);
+      // 남은 이미지 계산
+      const remainImage = 10 - imageSrc.length;
+      if (files.length > remainImage) {
+        alert(
+          `⛔️ 최대 10장까지만 업로드 가능합니다. ⛔️\n\n` +
+            `✔️ 선택한 이미지 개수: ${files.length}장\n` +
+            `✔️ 선택 가능한 이미지 개수: ${remainImage}장`,
+        );
+      } else {
+        for (let i = 0; i < files.length; i++) {
+          const resizedImage = await resizeImage(files[i]);
+          setImageSrc((prevImages) => [...prevImages, resizedImage]);
+        }
+      }
+    }
+  };
+  console.log(activeTags);
+
+  // 이미지 삭제 처리
+  const handleDeleteImage = (index: number) => {
+    // 이미지 삭제 사 UI에서도 삭제
+    setImageSrc((prevImages) => prevImages.filter((_, i) => i !== index));
+    // 이미지 삭제 시 데이터에서도 삭제
+    setFileNames((prevFileNames) =>
+      prevFileNames.filter((_, i) => i !== index),
+    );
+  };
+
+  // ## 해시태그 관련 부분  ##
+  const toggleHashTagVisibility = () => {
+    setIsOpen(!isOpen); // 표시 상태 토글
+  };
+  const handleToggle = () => {
+    toggleHashTagVisibility(); // 해시태그 보이기/숨기기 토글
+    setIsRotated(!isRotated); // 회전 상태 토글
   };
 
   const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -317,6 +419,7 @@ const CreatePostPage = () => {
     });
   };
 
+  // ## axios ##
   // 폼 제출 이벤트 처리
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -343,82 +446,6 @@ const CreatePostPage = () => {
       });
   };
 
-  // 이미지 업로드 처리
-  const handleUploadClick = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    if (imageSrc.length >= 10) {
-      alert('최대 10장까지만 업로드 가능합니다.');
-      return;
-    }
-    fileInputRef.current?.click();
-  };
-
-  // 이미지 리사이징
-  const resizeImage = (file: File): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.onload = () => {
-        const canvas = document.createElement('canvas');
-        const maxDimension = 90; // 최대 너비와 높이
-        let newWidth, newHeight;
-
-        if (img.width > img.height) {
-          // 너비가 높이보다 클 경우, 너비를 기준으로 비율 조정
-          newWidth = maxDimension;
-          newHeight = (img.height / img.width) * maxDimension;
-        } else {
-          // 높이가 너비보다 크거나 같을 경우, 높이를 기준으로 비율 조정
-          newHeight = maxDimension;
-          newWidth = (img.width / img.height) * maxDimension;
-        }
-
-        canvas.width = newWidth;
-        canvas.height = newHeight;
-        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-        // 캔버스에 새로 계산된 크기로 이미지를 그립니다
-        ctx.drawImage(img, 0, 0, newWidth, newHeight);
-        resolve(canvas.toDataURL());
-      };
-      img.onerror = reject;
-      img.src = URL.createObjectURL(file);
-    });
-  };
-
-  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files) {
-      const fileArray = Array.from(files);
-      const newFileNames = fileArray.map((file) => file.name); // 파일 이름 추출
-      setFileNames(newFileNames);
-      // 남은 이미지 계산
-      const remainImage = 10 - imageSrc.length;
-      if (files.length > remainImage) {
-        alert(
-          `⛔️ 최대 10장까지만 업로드 가능합니다. ⛔️\n\n` +
-            `✔️ 선택한 이미지 개수: ${files.length}장\n` +
-            `✔️ 선택 가능한 이미지 개수: ${remainImage}장`,
-        );
-      } else {
-        for (let i = 0; i < files.length; i++) {
-          const resizedImage = await resizeImage(files[i]);
-          setImageSrc((prevImages) => [...prevImages, resizedImage]);
-        }
-      }
-    }
-  };
-  console.log(activeTags);
-
-  // 이미지 삭제 처리
-  const handleDeleteImage = (index: number) => {
-    // 이미지 삭제 사 UI에서도 삭제
-    setImageSrc((prevImages) => prevImages.filter((_, i) => i !== index));
-    // 이미지 삭제 시 데이터에서도 삭제
-    setFileNames((prevFileNames) =>
-      prevFileNames.filter((_, i) => i !== index),
-    );
-  };
-
   return (
     <form onSubmit={handleSubmit}>
       <CreatePostHeader>
@@ -427,8 +454,10 @@ const CreatePostPage = () => {
       </CreatePostHeader>
       <CreatePostBody>
         <FormTitle>새 게시물 작성</FormTitle>
+        {imageSrc.length > 0 && <ImageCount>{imageSrc.length}/10</ImageCount>}
+
         <UploadImageArea>
-          <HiddenI>
+          <HiddenImageContainer>
             <HiddenImageInput
               ref={fileInputRef}
               type="file"
@@ -438,17 +467,13 @@ const CreatePostPage = () => {
             />
             {imageSrc.map((src, index) => (
               <ImageDisplay key={src}>
-                <img
-                  src={src}
-                  alt={`Uploaded ${index}`}
-                  style={{ width: '100%', height: 'auto' }}
-                />
-                <DeleteIcon onClick={() => handleDeleteImage(index)}>
-                  ×
-                </DeleteIcon>
+                <StyledImage src={src} alt={`Uploaded ${src}`} />
+                <DeleteIconWrap onClick={() => handleDeleteImage(index)}>
+                  <DeleteIcon />
+                </DeleteIconWrap>
               </ImageDisplay>
             ))}
-          </HiddenI>
+          </HiddenImageContainer>
           {imageSrc.length === 0 && (
             <UploadImageLargeText>
               네일아트 디자인을 업로드해 보세요.
@@ -482,7 +507,7 @@ const CreatePostPage = () => {
               onKeyDown={handleKeyDown}
             />
             <HashTagToggleIconWrap>
-              <HashTagToggleIcon  onClick={toggleHashTagVisibility} isRotated={isRotated}>
+              <HashTagToggleIcon onClick={handleToggle} $isRotated={isRotated}>
                 <ToggleArrowIcon />
               </HashTagToggleIcon>
             </HashTagToggleIconWrap>
@@ -498,7 +523,7 @@ const CreatePostPage = () => {
             ))}
           </HashtagCreate>
 
-          {isOpen && (
+          {!isOpen && (
             <HashTagButton activeTags={activeTags} toggleTag={toggleTag} />
           )}
         </ContentsAndHashTagContainer>
