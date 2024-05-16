7390e80371d7c9d0399fb2b353c1356d9652b49e
LKW9 <sg4582@naver.com>
2024-05-16 12:29:45 +0900

    Feat: 내 아카이브 생성 모달 구현, Refactor : 아카이브 게시물 요청 추가

--- frontend/src/constant/colors.tsx
+++ frontend/src/constant/colors.tsx
@@ -4,3 +4,4 @@ export const DARK_PURPLE = '#786387';
 export const TEXT_BLACK = '#141217';
 export const INPUT_LIGHTGRAY = '#E0DBE5';
 export const WARNING_TEXT = '#FF007A';
+export const INPUT_TEXT = '#756982'
