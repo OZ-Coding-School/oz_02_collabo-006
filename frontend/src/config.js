71c567e5a6d7a6583419d7dffeabc96010984466
bamjun <khy5116@naver.com>
2024-05-19 03:11:45 +0900

    fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://web:8000';
 
 export default API_BASE_URL;
