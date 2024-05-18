a3a5e944e12290bc63d3ac4df0acef9b809dba68
bamjun <khy5116@naver.com>
2024-05-19 02:24:10 +0900

    fix

--- frontend/src/constant/endPoint.tsx
+++ frontend/src/constant/endPoint.tsx
@@ -1,12 +1,12 @@
-export const CREATE_USER_ENDPOINT = 'http://127.0.0.1:8000/api/v1/user/create/';
+import API_BASE_URL from '../config';
+
+export const CREATE_USER_ENDPOINT = `${API_BASE_URL}/api/v1/user/create/`;
 export const UPDATE_USER_ENDPOINT = (userId: Number) => {
-  return `http://127.0.0.1:8000/api/v1/user/${userId}/update/`;
+  return `${API_BASE_URL}/api/v1/user/${userId}/update/`;
 };
-export const LOGIN_USER_ENDPOINT = 'http://127.0.0.1:8000/api/v1/user/login/';
-export const LOGOUT_USER_ENDPOINT = 'http://127.0.0.1:8000/api/v1/user/logout/';
-export const TOKEN_USER_ENDPOINT =
-  'http://127.0.0.1:8000/api/v1/user/login/sjwt/';
-export const REFRESH_TOKEN_ENDPOINT =
-  'http://127.0.0.1:8000/api/v1/user/jwt/refresh/';
-export const CREATE_POSTS = 'http://127.0.0.1:8000/api/v1/post/create/';
-export const GET_ALL_POSTS = 'http://127.0.0.1:8000/api/v1/post/';
+export const LOGIN_USER_ENDPOINT = `${API_BASE_URL}/api/v1/user/login/`;
+export const LOGOUT_USER_ENDPOINT = `${API_BASE_URL}/api/v1/user/logout/`;
+export const TOKEN_USER_ENDPOINT = `${API_BASE_URL}/api/v1/user/login/sjwt/`;
+export const REFRESH_TOKEN_ENDPOINT = `${API_BASE_URL}/api/v1/user/jwt/refresh/`;
+export const CREATE_POSTS = `${API_BASE_URL}/api/v1/post/create/`;
+export const GET_ALL_POSTS = `${API_BASE_URL}/api/v1/post/`;
